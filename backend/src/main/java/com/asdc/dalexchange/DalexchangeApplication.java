063fe7f2246256fc0c74d8ce4333f0797d648e4f
rvaghani <rt691750@dal.ca>
2024-08-14 11:36:51 -0300

    DalExchange project added

---
+++ backend/src/main/java/com/asdc/dalexchange/DalexchangeApplication.java
@@ -0,0 +1,14 @@
+package com.asdc.dalexchange;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DalexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalexchangeApplication.class, args);
+	}
+}
